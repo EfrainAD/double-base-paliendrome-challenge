6531c54f8635eaf1374d9aa4f24c31630b73f80f
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-29 09:41:11 -0400

    cleaned up the code.

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -20,47 +20,22 @@ This question is from - https://projecteuler.net/problem=36
 */
 const doubleBasePalindromeSum = function (x,y,upperLimit) {
       const start = upperLimit
-      const palindromic = []
       let palindromicSum = 0
       let base10 = 0
       let based2 = 0
-      let odd25NotGivenInDirections = 0
       
       for (let i = 0; i < start; i++) {
-      
-            //check if based 10 is palindromic
-            // console.log(base10)
             const based10Reversed = base10.toString().split('').reverse().join('')
-            // console.log(based10Reversed)
             if (base10 == based10Reversed) {
-                  // IF yes, convert to 2 base
                   based2 = base10.toString(2)
-                  // based2 = base10.toString(2)
-                  // console.log(base10)
-                  //check if palindromic
                   const based2Reversed = based2.toString().split('').reverse().join('')
                   if (based2 == based2Reversed) {
-                        // IF yes, add to palindromic array
-                        // console.log(based2)
                         palindromicSum += base10
-                        if (i <= 10) {
-                              odd25NotGivenInDirections += base10
-                        }
                   }
             }
             base10++
       }
-      // console.log('base10: ',palindromicSum)
-      // console.log(palindromicSum.toString(2))
-      // console.log('odd: ',odd25NotGivenInDirections)
-      // return (palindromicSum, odd25NotGivenInDirections)
-      // return (odd25NotGivenInDirections, palindromicSum)
-      console.log(upperLimit)
-      console.log('HI ' ,palindromicSum)
-      return (palindromicSum)
-      // 
-      // palindromicSum.toString(2)
-      // 
+      return(palindromicSum)
 }
 
 // DO NOT MODIFY
